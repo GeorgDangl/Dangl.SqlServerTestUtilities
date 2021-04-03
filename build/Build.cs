a13f4b6562b41a6f90587080a6714da615d1fb01
Georg Dangl <georg@dangl.me>
2021-04-03 23:53:16 +0200

    Configure documentation generation

--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
@@ -7,36 +5,47 @@ using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DocFX;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using Nuke.WebDocu;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DocFX.DocFXTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.GitHub.ChangeLogExtensions;
+using static Nuke.WebDocu.WebDocuTasks;
 
 [CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+    /// - JetBrains ReSharper https://nuke.build/resharper
+    /// - JetBrains Rider https://nuke.build/rider
+    /// - Microsoft VisualStudio https://nuke.build/visualstudio
+    /// - Microsoft VSCode https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Parameter] string DocuBaseUrl;
+    [Parameter] string DocuApiKey;
+
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion] readonly GitVersion GitVersion;
+    [GitVersion(Framework = "net5.0")] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
     AbsolutePath OutputDirectory => RootDirectory / "output";
+    AbsolutePath DocFxFile => DocsDirectory / "docfx.json";
+    AbsolutePath ChangelogFile => RootDirectory / "CHANGELOG.md";
+    AbsolutePath ReadmeFile => RootDirectory / "README.md";
+    AbsolutePath DocsDirectory => RootDirectory / "docs";
 
     Target Clean => _ => _
         .Before(Restore)
@@ -67,4 +76,44 @@ class Build : NukeBuild
                 .EnableNoRestore());
         });
 
+    Target BuildDocFxMetadata => _ => _
+        .DependsOn(Clean)
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DocFXMetadata(x => x
+                .SetProcessEnvironmentVariable("DOCFX_SOURCE_BRANCH_NAME", GitVersion.BranchName)
+                .SetProjects(DocFxFile));
+        });
+
+    Target BuildDocumentation => _ => _
+        .DependsOn(Clean)
+        .DependsOn(BuildDocFxMetadata)
+        .Executes(() =>
+        {
+            CopyFile(ReadmeFile, DocsDirectory / "index.md");
+            CopyFile(ChangelogFile, DocsDirectory / "CHANGELOG.md");
+            DocFXBuild(x => x
+                .SetProcessEnvironmentVariable("DOCFX_SOURCE_BRANCH_NAME", GitVersion.BranchName)
+                .SetConfigFile(DocFxFile));
+            DeleteFile(DocsDirectory / "index.md");
+            DeleteFile(DocsDirectory / "CHANGELOG.md");
+        });
+
+    Target UploadDocumentation => _ => _
+        .DependsOn(BuildDocumentation)
+        .Requires(() => DocuApiKey)
+        .Requires(() => DocuBaseUrl)
+        .Executes(() =>
+        {
+            var changeLog = GetCompleteChangeLog(ChangelogFile);
+
+            WebDocu(s => s
+                .SetDocuBaseUrl(DocuBaseUrl)
+                .SetDocuApiKey(DocuApiKey)
+                .SetMarkdownChangelog(changeLog)
+                .SetSourceDirectory(OutputDirectory / "docs")
+                .SetVersion(GitVersion.NuGetVersion)
+                .SetSkipForVersionConflicts(true));
+        });
 }
